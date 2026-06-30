c733fc57441c1d29e48992e8ec4403551fa8fad3
janaStani <stanisavoskajana@gmail.com>
2026-06-30 13:21:37 +0200

    remove the right unused file

--- sys3-project-main/front-end/src/Utils/Constants.js
+++
@@ -1,7 +0,0 @@
-export const HOME    = 'home';
-export const MY_CAR  = 'mycar';
-export const RATING  = 'rating';
-export const ABOUT   = 'about';
-export const SIGNUP  = 'signup';
-export const LOGIN   = 'login';
-export const LOGOUT  = 'logout';
